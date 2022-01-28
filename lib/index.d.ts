42d2e4393070deaa87158fc081e89808143b3014
Abir <abir.pal899@gmail.com>
2022-01-28 09:28:40 +0530

    Add Typescript Support for Bridge

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,9 @@
 import "storyblok-js-client/types/index";
+declare global {
+    interface Window{
+        StoryblokBridge: FunctionConstructor
+    }
+    interface Function{
+        on:Function
+    }
+}
