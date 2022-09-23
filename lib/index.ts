4a39693c507f41ef9250f6d2ffe78d6f58a7d152
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-09-23 13:59:01 +0200

    fix: add resolverInstance for Nuxt, Astro and other SDKs

--- lib/index.ts
+++ lib/index.ts
@@ -110,9 +110,11 @@ const setComponentResolver = (resolver, resolveFn) => {
 
 export const renderRichText = (
   data: Richtext,
-  options?: SbRichTextOptions
+  options?: SbRichTextOptions,
+  resolverInstance?: any,
 ): string => {
-  if (!richTextResolver) {
+  let localResolver = resolverInstance || richTextResolver;
+  if (!localResolver) {
     console.error(
       "Please initialize the Storyblok SDK before calling the renderRichText function"
     );
@@ -128,7 +130,6 @@ export const renderRichText = (
     return "";
   }
 
-  let localResolver = richTextResolver;
   if (options) {
     localResolver = new RichTextResolver(options.schema);
     if (options.resolver) {
