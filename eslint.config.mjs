73cad44bb87be18a4fa1961ea894de41b224ca97
alvarosabu <alvaro.saburido@gmail.com>
2024-12-05 10:39:56 +0100

    chore: explicitely set vue plugin for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { storyblokLintConfig } from '@storyblok/eslint-config';
 
 export default storyblokLintConfig({
-
+  vue: true,
 }, {
   ignores: ['tests/unit/coverage/', 'dist/', 'cypress/'],
 });
