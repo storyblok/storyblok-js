f60a7c05380326b4f53c40f0163819a0da6fed90
Edoardo Dusi <edo@edoardodusi.com>
2024-12-24 16:56:36 +0100

    fix: wrong paths in exports, update to vite confs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,5 @@ import { storyblokLintConfig } from '@storyblok/eslint-config';
 export default storyblokLintConfig({
   vue: true,
 }, {
-  ignores: ['tests/unit/coverage/', 'dist/', 'cypress/', '**/*.d.ts'],
+  ignores: ['tests/unit/coverage/', 'dist/', 'cypress/', '**/*.d.ts', '**/node_modules/**', 'README.md'],
 });
